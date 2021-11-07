f9b2b9e9fc27fa253280bd5a0d5e835e4f3f68b5
Peter K <pkjar16@student.aau.dk>
2021-11-07 22:06:05 +0100

    Update pytorch.sh

--- pytorch.sh
+++ pytorch.sh
@@ -1,5 +1,4 @@
 pip install -r pytorch-requirements.txt
 wget http://95.216.163.151/Roms.zip
-mkdir ROM/
-unzip Roms.zip ROM/
-python -m atari_py.import_roms ROM/
+unzip Roms.zip
+python -m atari_py.import_roms Roms
